f3c78b9ccbcda94695c256c9d94c5e42efd45bc8
Jakub Wiraszka <WirachaPL@wp.pl>
2022-11-25 22:21:31 +0000

    List 7 Working Binary Tree

--- src/L/ocaml/Lista7.ml
+++ src/L/ocaml/Lista7.ml
@@ -35,14 +35,14 @@ Segment.length Segment.{a = point1; b = point2};;
 
 (* ZAD 3 *)
 
-module type BT_type = sig
+(* module type BT_type = sig
   type 'a bt = Empty | Node of 'a * 'a bt * 'a bt;;
   val add : 'a bt -> 'a -> 'a bt;;
   val remove : 'a bt -> 'a -> 'a bt;;
   val list : 'a bt -> 'a list;;
-end;;
+end;; *)
 
-module BT : BT_type =
+(* module BT : BT_type =
 struct
   type 'a bt = Empty | Node of 'a * 'a bt * 'a bt;;
   (* let rec add (tt: 'a bt) v =
@@ -103,9 +103,122 @@ struct
       | (_, Empty) -> l
       | (l, r) -> let lastVal = (last tt) in set (removeHelper tt lastVal) v lastVal in
       removeHelper tt v;;
+end;; *)
+
+module type BT_type = sig
+  type 'a bt = Empty | Node of 'a node and 'a node = { mutable value: 'a; mutable parent: 'a bt;  mutable left: 'a bt; mutable right: 'a bt };;
+  val add : 'a bt -> 'a -> unit;;
+  val remove : 'a bt -> 'a -> unit;;
+  val preorder : 'a bt -> 'a list;;
+  val inorder : 'a bt -> 'a list;;
+  val postorder : 'a bt -> 'a list;;
+  val leafList : 'a bt -> 'a bt list;;
 end;;
 
-let tt = BT.add (BT.add (BT.add (BT.Node(2, BT.Empty, BT.Empty)) 1) 3) 4;;
-BT.list tt;;
-let tt = BT.remove tt 2;;
-BT.list tt;;
+module BT : BT_type =
+struct
+type 'a bt = Empty | Node of 'a node and 'a node = { mutable value: 'a; mutable parent: 'a bt; mutable left:  'a bt; mutable right: 'a bt }
+  let add (tt: 'a bt) nv =
+    let rec findPlace q =
+      match q with
+      | [] -> failwith "Error"
+      | h::tl ->
+        match h with
+        | Empty -> findPlace tl
+        | Node {value; left; right} ->
+          match (left, right) with
+          | (Empty, _) -> value
+          | (_, Empty) -> value
+          | (left, right) -> findPlace (tl@[left; right]) and
+      addNode (Node n) tt v b =
+      if b then n.left <- Node{value=v; parent=tt; left=Empty; right=Empty} else 
+        n.right <- Node{value=v; parent=tt; left=Empty; right=Empty} in
+      let rec addHelper (tt: 'a bt) p =
+        match tt with
+        | Empty -> ()
+        | Node {value; left; right} when value <> p -> addHelper left p; addHelper right p
+        | Node {value; left; right} ->
+          match (left, right) with
+          | (Empty, _) -> addNode tt tt nv true
+          | (_, Empty) -> addNode tt tt nv false
+          | (left, right) -> () in
+          addHelper tt (findPlace [tt]);;
+  let set (tt: 'a bt) o n =
+    let setValue (Node n) v =
+      n.value <- v in
+    let rec setHelper (tt: 'a bt) =
+      match tt with
+      | Empty -> ()
+      | Node {value; left; right} ->
+        if value = o then setValue tt n else setHelper left; setHelper right in
+    setHelper tt;;
+  let last (tt: 'a bt) =
+    let rec lastHelper q acc =
+      match q with
+      | [] -> acc
+      | h::tl ->
+        match h with
+        | Empty -> lastHelper tl h
+        | Node{value; left; right} ->
+          match (left, right) with
+          | (Empty, Empty) -> lastHelper tl h
+          | (left, Empty) -> lastHelper (tl@[left]) acc
+          | (Empty, right) -> lastHelper (tl@[right]) acc
+          | (left, right) -> lastHelper (tl@[left; right]) acc in
+          lastHelper [tt] tt;;
+  let rec preorder (tt: 'a bt) =
+    match tt with
+    | Empty -> []
+    | Node {value; left; right} -> [value] @ preorder left @ preorder right;;
+  let rec inorder (tt: 'a bt) = 
+    match tt with
+    | Empty -> []
+    | Node {value; left; right} -> inorder left @ [value] @ inorder right;;
+  let rec postorder (tt: 'a bt) = 
+    match tt with
+    | Empty -> []
+    | Node {value; left; right} -> postorder left @ postorder right @ [value];;
+  (* let leafList (tt: 'a bt) =
+    let rec leafHelper q acc =
+      match q with
+      | [] -> List.rev acc
+      | h::tl ->
+        match h with
+        | Empty -> leafHelper tl acc
+        | Node{value; left; right} ->
+          match (left, right) with
+          | (Empty, Empty) -> leafHelper tl (h::acc)
+          | (left, right) -> leafHelper (tl@[left; right]) acc in
+          leafHelper [tt] [];; *)
+  let rec leafList (tt: 'a bt) =
+    match tt with
+    | Empty -> []
+    | Node {value; left; right} ->
+      match (left, right) with
+      | (Empty, Empty) -> [tt]
+      | (Empty, right) -> leafList right
+      | (left, Empty) -> leafList left
+      | (left, right) -> leafList left@leafList right;;
+  let remove (tt: 'a bt) v =
+    let getValue (Node n) =
+      n.value in
+    let deleteChild (Node n) v =
+      if (getValue n.left) = v then n.left <- Empty else n.right <- Empty in
+    match last tt with
+    | Empty -> ()
+    | Node {value; parent} ->
+    set tt v value; 
+    deleteChild parent value;;
+end;;
+
+let tt = BT.Node{value=1; parent=Empty; left=Empty; right=Empty};;
+BT.add tt 2;;
+BT.add tt 3;;
+BT.add tt 4;;
+BT.add tt 5;;
+BT.inorder tt;;
+BT.postorder tt;;
+BT.leafList tt;;
+BT.preorder tt;;
+BT.remove tt 2;;
+BT.preorder tt;;
